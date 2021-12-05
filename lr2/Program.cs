48fd1cc786ef108e43ebb6b6803931450820c02c
Ксюша Рыбьякова <rybyakova.kseniya@mail.ru>
2021-12-05 21:54:36 +0300

    initial

---
+++ lr2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            double Num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите операцию: ");
+            string zh = Console.ReadLine();
+            Console.WriteLine("Введите число: ");
+            double Num2 = Convert.ToDouble(Console.ReadLine());
+            double res = double.NaN;
+
+            if (zh == "+")
+                res = Num1 + Num2;
+            if (zh == "-")
+                res = Num1 - Num2;
+            if (zh == "/")
+                res = Num1/Num2;
+            if (zh == "*")
+                res = Num1*Num2;
+            Console.WriteLine(res);
+        }
+    }
+}
